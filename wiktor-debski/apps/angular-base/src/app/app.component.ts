67d00009ac6dbd609c8c6da5681a18bfd2826c9a
Wiktor Debski <dewiktor@gmail.com>
2021-03-14 10:22:36 +0100

    base nx configuration

---
+++ wiktor-debski/apps/angular-base/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'wiktor-debski-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'angular-base';
+}
